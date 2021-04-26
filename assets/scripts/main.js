0b5863370a7bb691134c7c4723a1e56289ac8794
rewoop <10rubleqow@gmail.com>
2021-04-26 22:02:13 +0300

    init

---
+++ assets/scripts/main.js
@@ -0,0 +1,17 @@
+let scene = new Phaser.Scene('Game');
+
+scene.preload = () => {
+
+};
+
+scene.create = () => {
+
+};
+
+let config = {
+    type: Phaser.AUTO, // webgl or canvas (if webgl doesn't work)
+    width: 1920,
+    height: 1080,
+    scene: scene
+};
+let game = new Phaser.Game(config);
