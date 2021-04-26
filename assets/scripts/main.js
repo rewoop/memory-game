74a37f950451430062284a399697cafdd455ab19
rewoop <10rubleqow@gmail.com>
2021-04-26 23:09:58 +0300

    adds background

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,8 +1,13 @@
 let scene = new Phaser.Scene('Game');
 
-scene.preload = () => {};
+scene.preload = () => {
+	scene.load.image('bg', 'assets/sprites/background.jpeg');
+};
 
-scene.create = () => {};
+scene.create = () => {
+	//scene.add.sprite(scene.sys.game.config.width / 2, scene.sys.game.config.height / 2, 'bg');
+	scene.add.sprite(0, 0, 'bg').setOrigin(0, 0);
+};
 
 let config = {
 	type: Phaser.AUTO, // webgl or canvas (if webgl doesn't work)
@@ -12,3 +17,5 @@ let config = {
 };
 
 let game = new Phaser.Game(config);
+
+//TODO: сделать вебпак сборку, чекнуть статью на хабре (вкладка "что почитать")
